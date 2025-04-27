3db8a9d3d4b6d1b1b9e82448ca5b43337cb0f68d
Vitalij <krinickij.v.a@gmail.com>
2025-04-27 16:27:09 +0300

    FIX: fix form error
    Исправлена ошибка при загрузке формы создания  пользователя
    Ранее при загрузке страницы сразу отрабатывала ошибка формы

--- controller/UserListController.php
+++ controller/UserListController.php
@@ -6,24 +6,27 @@ $title='UserListPage';
 $titleH1 = 'UserListPage';
 $error=null;
 $userList = new UserProvider($pdo)->getAllUsers();
-
-
-if(isset($_POST['name'], $_POST['login'], $_POST['password'] )){
-    $user = new User($_POST['name']);
-    $user->setLogin($_POST['login']);
-    $userProvider = new UserProvider($pdo);
-    $userProvider->registerUser($user, $_POST['password']);
-    header('Location : /?refresh='.time());
-}else{
-    $error = 'Некорректные данные';
+if (isset($_POST['submit'])){
+    if(isset($_POST['name'], $_POST['login'], $_POST['password'] )){
+        $user = new User($_POST['name']);
+        $user->setLogin($_POST['login']);
+        $userProvider = new UserProvider($pdo);
+        $userProvider->registerUser($user, $_POST['password']);
+        header('Location: /?refresh='.time());
+        exit;
+    }else{
+        $error = 'Некорректные данные';
+    }
 }
 
-if (isset($_GET['delete'])){
-    var_dump($_GET['delete']);
-    $userProvider = new UserProvider($pdo);
-    $result = $userProvider->deleteUserById($_GET['delete']);
-    header('Location : /?controller=userlist');
 
-    
+if (isset($_GET['delete'])){
+    $userProvider = new UserProvider($pdo);
+    if ($userProvider->deleteUserById($_GET['delete'])) {
+        header('Location: /?controller=userlist');  // Убрал пробел после Location
+        exit;
+    } else {
+        $error = 'Ошибка при удалении пользователя';
+    }
 }
 include_once('views/userList.php');
